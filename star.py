f8700de723854c34d57af90c0ff039c04333db28
ßingen <bingen@voltanet.io>
2017-10-10 17:07:46 +0200

    Add star topology python script

--- star.py
+++ star.py
@@ -9,29 +9,34 @@ def generate_zebra_config_file(i, nodes, folder):
     with open(folder+"/zebra-" + str(i) + ".conf", "w") as f:
         f.write("interface lo\n")
         f.write(" ip address 1.0.0." + str(i) + "/32\n")
-        for j in range(1, nodes+1):
-            if i == j:
-                continue
-            f.write("interface " + str(i) + "-xge"+ str(j) + "\n")
-            f.write(" ip address 1." + get_ip_body(i, j) + "." + str(i) + "/24\n")
+        if i == 1:
+            for j in range(2, nodes+1):
+                f.write("interface 1-xge"+ str(j) + "\n")
+                f.write(" ip address 1.1." + str(j) + "." + str(i) + "/24\n")
+        else:
+            f.write("interface "+ str(i) + "-xge1\n")
+            f.write(" ip address 1.1." + str(i) + "." + str(i) + "/24\n")
 def generate_ospf_config_file(i, nodes, folder):
     with open(folder+"/ospfd-" + str(i) + ".conf", "w") as f:
         f.write("router ospf\n")
         f.write(" ospf router-id 1.0.0." + str(i) + "\n")
         f.write(" network 1.0.0." + str(i) + "/32 area 0.0.0.0\n")
-        for j in range(1, nodes+1):
-            if i == j:
-                continue
-            f.write(" network 1." + get_ip_body(i, j) + ".0/24 area 0.0.0.0\n")
+        if i == 1:
+            for j in range(2, nodes+1):
+                f.write(" network 1.1." + str(j) + ".0/24 area 0.0.0.0\n")
+        else:
+            f.write(" network 1.1." + str(i) + ".0/24 area 0.0.0.0\n")
 def generate_bgp_config_file(i, nodes, folder):
     BGP_ID = "65000"
     with open(folder+"/bgpd-" + str(i) + ".conf", "w") as f:
         f.write("router bgp " + BGP_ID + "\n")
         f.write(" bgp router-id 1.0.0." + str(i) + "\n")
-        for j in range(1, nodes+1):
-            if i == j:
-                continue
-            f.write(" neighbor 1." + get_ip_body(i, j) + "." + str(j) + \
+        if i == 1:
+            for j in range(2, nodes+1):
+                f.write(" neighbor 1.1." + str(j) + "." + str(j) + \
+                        " remote-as " + BGP_ID + "\n")
+        else:
+            f.write(" neighbor 1.1." + str(i) + ".1" + \
                     " remote-as " + BGP_ID + "\n")
 
 def generate_config_files(nodes, folder):
@@ -41,11 +46,10 @@ def generate_config_files(nodes, folder):
         generate_bgp_config_file(i, nodes, folder)
 
 def generate_dot_file(nodes):
-    with open("full_mesh_" + str(nodes) + ".dot", 'w') as f:
+    with open("star_" + str(nodes) + ".dot", 'w') as f:
         f.write("graph test {\n")
-        for i in range(1, nodes+1):
-            for j in range (i+1, nodes+1):
-                f.write(str(i) + " -- " + str(j) + "\n")
+        for j in range (2, nodes+1):
+            f.write("1 -- " + str(j) + "\n")
         f.write("}\n")
 
 def main(args):
